9fd5a121e76ad427fb6551536cfbabf3541d981e
Prathamesh <prathmeshbhujade1234@gmail.com>
2026-02-02 12:03:20 +0530

    EC2 module added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-module "EC2" {
-  source        = "./EC2"
+module "ec2" {
+  source        = "git::https://github.com/prathamesh633/Terraform-tasks.git//EC2?ref=v1.0"
   ami_id        = "ami-073130f74f5ffb161"
   instance_type = "t3.micro"
   server_name   = "new_server"
